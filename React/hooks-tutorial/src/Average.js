cd124e204072479dd892673d86a2678cb9b73b82
wish1030 <wish@khu.ac.kr>
2022-01-09 21:32:55 +0900

    Feat: useCallback 공부

--- React/hooks-tutorial/src/Average.js
+++ React/hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중...");
@@ -11,14 +11,20 @@ const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
 
-  const onChange = (e) => {
+  //useCallback 사용 x -> 컴포넌트가 리렌더링될때마다 새로 만들어진 함수 사용.
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
-    const nextList = list.concat(parseInt(number)); //concat : 파라미터값을 기존 배열에 합쳐서 새로운 배열 생성
-    setList(nextList);
-    setNumber("");
-  };
+  }, []); //컴포넌트가 처음 렌더링될때만 함수 생성
+
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number)); //concat : 파라미터값을 기존 배열에 합쳐서 새로운 배열 생성
+      setList(nextList);
+      setNumber("");
+    },
+    [number, list]
+  ); //number 나 list가 바뀌었을때만 함수 생성
+  //함수 내부에서 상태값에 의존해야 할때는 그 값을 반드시 배열안에 포함시켜야함.
 
   //list배열의 내용이 바뀔때만  getAverage 함수가 호출됨.
   //useMemo : 렌더링하는 과정에서 특정 값이 바뀌었을때만 연산 실행, 원하는 값이 바뀌지 않았다면 이전에 연산했던 결과를 다시 사용
