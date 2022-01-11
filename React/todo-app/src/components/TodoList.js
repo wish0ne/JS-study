e0526a80063309fdda28dedef333052b84c16c9f
wish1030 <wish@khu.ac.kr>
2022-01-11 22:19:14 +0900

    컴포넌트 성능 최적화 : react-virtualized를 사용한 렌더링 최적화

--- React/todo-app/src/components/TodoList.js
+++ React/todo-app/src/components/TodoList.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
+  //react-virtualized의 List 컴포넌트에서 각 TodoItem을 렌더링할때 사용하는 함수
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+  return (
+    <List
+      className="TodoList"
+      width={512} //전체 크기
+      height={513} //전체 높이
+      rowCount={todos.length} //항목 개수
+      rowHeight={57} //항목 높이
+      rowRenderer={rowRenderer} //항목을 렌더링할때 쓰는 함수
+      list={todos} //배열
+      dstyle={{ outline: 'none' }} //List에 기본 적용되는 outline 스타일 제거
+    />
   );
 };
 
-export default TodoList;
+//리스트에 관련된 컴포넌트를 최적화할때는 리스트 내부에서 사용하는 컴포넌트도 최적화하고, 리스트로 사용되는 컴포넌트 자체도 최적해주는것이 좋음.
+export default React.memo(TodoList);
--- React/todo-app/src/components/TodoListItem.js
+++ React/todo-app/src/components/TodoListItem.js
@@ -7,20 +7,28 @@ import {
 import cn from 'classnames'; //조건부 스타일링 위해 classnames 사용
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
 //TodoListItem 컴포넌트는 props(todo, onRemove, onToggle)이 바뀌지 않으면 리렌더링 하지 않음.
-export default React.memo(TodoListItem);
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
