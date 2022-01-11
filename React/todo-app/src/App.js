e7998f5d37afa65cdc0af46bd8f71512c771bdfe
wish1030 <wish@khu.ac.kr>
2022-01-11 14:22:32 +0900

    일정 관리 웹 애플리케이션 만들기 : App.js에서 todos state들 관리

--- React/todo-app/src/App.js
+++ React/todo-app/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
+//일정 항목에 대한 상태들은 모두 App에서 관리 (부모 컴포넌트에서)
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
