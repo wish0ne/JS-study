bb0e8f9aa2cdc1869f61cff47ed29f2f5f7ad001
wish1030 <wish@khu.ac.kr>
2022-01-11 14:46:48 +0900

    일정 관리 웹 애플리케이션 만들기 : 항목 추가 기능 구현

--- React/todo-app/src/App.js
+++ React/todo-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -22,9 +22,28 @@ const App = () => {
       checked: false,
     },
   ]);
+
+  //고유값으로 사용될 id. ref를 사용하여 변수담기
+  //id값은 렌더링되는 정보가 아니므로 useRef를 사용하여 변수 생성
+  const nextId = useRef(4);
+
+  //todos 배열에 새 항목 추가 함수
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
--- React/todo-app/src/components/TodoInsert.js
+++ React/todo-app/src/components/TodoInsert.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState(''); //입력 input
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  //onClick 대신 onSubmit 이벤트 사용한 이유 : onSubmit 이벤트는 인풋에서 엔터를 눌렀을때도 발생.
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+
+      //submit 이벤트는 브라우저에서 새로고침을 발생시킴.
+      //이를 방지하기 위한 함수 호출
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
