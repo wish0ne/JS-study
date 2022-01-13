5dc6a64cb8924fda50c4cc4a52481877b54b2f3a
wish1030 <wish@khu.ac.kr>
2022-01-13 17:24:13 +0900

    외부 API를 연동하여 뉴스 뷰어 만들기 : axios로 API 호출해서 데이터 받아오기

--- React/news-viewer/src/App.js
+++ React/news-viewer/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    //axios.get() : 파라미터로 전달된 주소에 GET 요청 해줌. 이에 대한 결과는 .then을 통해 비동기적으로 확인 가능.
+    // axios
+    //   .get('https://jsonplaceholder.typicode.com/todos/1')
+    //   .then((response) => {
+    //     setData(response.data);
+    //   });
+
+    //async 적용
+    try {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          // stringify(JSON문자열로 변환할 값, null이면 객체의 모든 속성이 JSON 문자열 결과에 저장, 공백으로 사용되는 스페이스의 수) : js값이나 객체를 JSON 문자열로 변환
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
