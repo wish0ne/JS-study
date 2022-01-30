3b751870ba959e7bf12ac077127449b28ce8cc7a
wish1030 <wish@khu.ac.kr>
2022-01-30 20:46:37 +0900

    리덕스를 사용하여 리액트 애플리케이션 상태 관리하기 : immer를 사용하여 리덕스 더 편하게 사용하기

--- React/react-redux-tutorial/src/modules/todos.js
+++ React/react-redux-tutorial/src/modules/todos.js
@@ -1,4 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 // 1. 액션 타입 정의
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
@@ -51,23 +52,29 @@ const initialState = {
 // createAction으로 만든 액션 생성 함수는 파라미터로 받아온 값을 객체 안에 넣을때 action.payload라는 이름을 공통적으로 넣어주게 됨.
 //따라서 모두 action.payload값을 조회하여 업데이트하도록 구현해줘야한다.
 //모든 추가 데이터값을 action.payload로 사용하기 때문에 코드가 헷갈릴 수 있음. => 객체 비구조화 할당 문법으로 action값의 payload일므을 새로 설정해주면 의미파악 쉬움.
+
+//immer를 사용한다고 해서 모든 업데이트 함수에 immer 적용할 필요 없음.
+//아래 역시 toggle을 제외한 업데이트 함수들은 immer를 쓰지 않는 코드가 더 짧기 때문에 이전 형태를 유지하는것도 무방하다.
 const todos = handleActions(
   {
-    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input: input }),
-    [INSERT]: (state, { payload: todo }) => ({
-      ...state,
-      todos: state.todos.concat(todo),
-    }),
-    [TOGGLE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.map((todo) =>
-        todo.id === id ? { ...todo, done: !todo.done } : todo,
-      ),
-    }),
-    [REMOVE]: (state, { payload: id }) => ({
-      ...state,
-      todos: state.todos.filter((todo) => todo.id !== id),
-    }),
+    [CHANGE_INPUT]: (state, { payload: input }) =>
+      produce(state, (draft) => {
+        draft.input = input;
+      }),
+    [INSERT]: (state, { payload: todo }) =>
+      produce(state, (draft) => {
+        draft.todos.push(todo);
+      }),
+    [TOGGLE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const todo = draft.todos.find((todo) => todo.id === id);
+        todo.done = !todo.done;
+      }),
+    [REMOVE]: (state, { payload: id }) =>
+      produce(state, (draft) => {
+        const index = draft.todos.findIndex((todo) => todo.id === id);
+        draft.todos.splice(index, 1);
+      }),
   },
   initialState,
 );
