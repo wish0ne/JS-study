3c001998c5bd1981127dcf1e59887372d82a28af
wish1030 <wish@khu.ac.kr>
2022-01-31 21:49:37 +0900

    리덕스 미들웨어를 통한 비동기 작업 관리 : 리덕스 코드 준비

--- React/learn-redux-middleware/src/App.js
+++ React/learn-redux-middleware/src/App.js
@@ -1,21 +1,12 @@
-function App() {
+import React from "react";
+import CounterContainer from "./containers/CounterContainer";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CounterContainer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ React/learn-redux-middleware/src/modules/counter.js
@@ -0,0 +1,20 @@
+import { createAction, handleActions } from "redux-actions";
+
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
+
+//상태는 꼭 객체일 필요 없음. 숫자도 작동.
+const initialState = 0;
+
+const counter = handleActions(
+  {
+    [INCREASE]: (state) => state + 1,
+    [DECREASE]: (state) => state - 1,
+  },
+  initialState
+);
+
+export default counter;
