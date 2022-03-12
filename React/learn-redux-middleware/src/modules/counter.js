39ebaf1bc8836f2cda93daf996a2b56c0a19f7b3
wish1030 <wish@khu.ac.kr>
2022-03-12 23:11:32 +0900

    리덕스 미들웨어를 통한 비동기 작업 관리 : redux-saga 유용한 기능들(select, throttle)

--- React/learn-redux-middleware/src/modules/counter.js
+++ React/learn-redux-middleware/src/modules/counter.js
@@ -1,5 +1,12 @@
 import { createAction, handleActions } from "redux-actions";
-import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
+import {
+  delay,
+  put,
+  takeEvery,
+  takeLatest,
+  select,
+  throttle,
+} from "redux-saga/effects";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
@@ -17,6 +24,9 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga() {
   yield delay(1000); //1초 기다림
   yield put(increase()); //특정 액션을 디스패치
+  //select를 사용하여 사가 내부에서 현재 상태를 참조
+  const number = yield select((state) => state.counter); //state는 스토어 상태를 의미함.
+  console.log(`현재 값은 ${number}입니다.`);
 }
 
 function* decreaseSaga() {
@@ -25,8 +35,9 @@ function* decreaseSaga() {
 }
 
 export function* counterSaga() {
-  //takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해줌.
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  //사가가 실행되는 주기를 제한가능. throttle을 사용하면 사가가 n초에 한번만 호출되도록 설정가능.
+  //3초에 한번만 increaseSaga가 호출됨.
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
   //takeLatest는 기존에 진행중이던 작업이 있다면 취소 처리하고 가장 마지막으로 실행된 작업만 수행
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
 }
