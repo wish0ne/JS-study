87a54ab75ad2e7860869d55225cefe02e93af447
wish1030 <wish@khu.ac.kr>
2022-03-14 00:43:30 +0900

    코드 스플리팅 : Loadable Components를 통한 코드 스플리팅

--- React/splitting-sample/src/App.js
+++ React/splitting-sample/src/App.js
@@ -1,21 +1,32 @@
-import React, { useState, Suspense } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-const SplitMe = React.lazy(() => import("./SplitMe"));
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  //로딩중에 다른 UI를 보고싶을때
+  fallback: <div>loading...</div>,
+});
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  //컴포넌트를 미리 불러오는 preload 방법.
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
+  //마우스를 Hello React!위에 올리기만 해도 로딩이 시작됨. (로딩만 하고 렌더링은 하지 않음)
+  //그리고 클릭했을때 렌더링됨.
+  //preload를 통해 더 좋은 UX 제공 가능!
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        <Suspense fallback={<div>loading...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
