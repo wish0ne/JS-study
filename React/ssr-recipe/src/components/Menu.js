bec11ab3cff54942245d3a82752f2c7610a75223
wish1030 <wish@khu.ac.kr>
2022-03-16 16:50:29 +0900

    서버 사이드 렌더링 : 컴포넌트 생성

---
+++ React/ssr-recipe/src/components/Menu.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+const Menu = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <Link to="/red">Red</Link>
+        </li>
+        <li>
+          <Link to="/blue">Blue</Link>
+        </li>
+      </ul>
+      <hr />
+      <Outlet />
+    </>
+  );
+};
+
+export default Menu;
