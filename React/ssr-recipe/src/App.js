752f6809935e9b08e38091ca7be4ce01bd213b4e
wish1030 <wish@khu.ac.kr>
2022-03-21 23:29:26 +0900

    서버 사이드 렌더링 : 데이터 로딩 - 컴포넌트 준비

--- React/ssr-recipe/src/App.js
+++ React/ssr-recipe/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Menu from "./components/Menu";
 import RedPage from "./pages/RedPage";
 import BluePage from "./pages/BluePage";
+import UsersPage from "./pages/UsersPage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Menu />}>
         <Route path="/red" element={<RedPage />} />
         <Route path="/blue" element={<BluePage />} />
+        <Route path="/users" element={<UsersPage />} />
       </Route>
     </Routes>
   );
--- React/ssr-recipe/src/components/Menu.js
+++ React/ssr-recipe/src/components/Menu.js
@@ -11,6 +11,9 @@ const Menu = () => {
         <li>
           <Link to="/blue">Blue</Link>
         </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
       </ul>
       <hr />
       <Outlet />
