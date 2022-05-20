aa9cd7443a270eceda9778085eba3460cf8715a2
wish0ne <wish@khu.ac.kr>
2022-05-20 20:42:27 +0900

    MongoDB 연동 : 데이터 삭제와 수정

--- React/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ React/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -60,10 +60,37 @@ export const read = async (ctx) => {
 /* 특정 포스트 제거
 DELETE /api/posts/:id
 */
-export const remove = (ctx) => {};
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; //No Content(성공하기는 했지만 응답할 데이터 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /* 포스트 수정(특정 필드 변경)
 PATCH /api/posts/:id
-{title, body}
+{
+  title : '제목',
+  body:'내용,
+  tags:['태그1', '태그2']
+}
 */
-export const update = (ctx) => {};
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, //이 값을 설정하면 업데이트된 데이터 반환
+      //false일때는 업데이트 되기 전의 데이터 반환
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
