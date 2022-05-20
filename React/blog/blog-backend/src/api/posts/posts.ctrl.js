087cde7a8f214630536d3b5258805b781785001c
wish0ne <wish@khu.ac.kr>
2022-05-20 20:37:26 +0900

    MongoDB 연동 : 데이터 생성과 조회

--- React/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ React/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,120 +1,69 @@
-let postId = 1; //id의 초기값
-
-//posts 배열 초기 데이터
-const posts = [
-  {
-    id: 1,
-    title: '제목',
-    body: '내용',
-  },
-];
+import Post from '../../models/post';
 
 /*포스트 작성
 POST /api/posts
-{title, body}
+{
+  title : '제목',
+  body:'내용,
+  tags:['태그1', '태그2']
+}
 */
-export const write = (ctx) => {
-  //REST API의 Request Body는 ctx.request.body에서 조회
-  const { title, body } = ctx.request.body;
-  postId += 1;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
+export const write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  //인스턴스 생성
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    //DB에 저장
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /* 포스트 목록 조회
 GET /api/posts
 */
-export const list = (ctx) => {
-  ctx.body = posts;
+export const list = async (ctx) => {
+  try {
+    //find()로 모델 인스턴스의 데이터 조회
+    //find()호출 후 exec()를 붙여줘야 서버에 쿼리를 요청함.
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /* 특정 포스트 조회
 GET /api/posts/:id
 */
-export const read = (ctx) => {
+export const read = async (ctx) => {
   const { id } = ctx.params;
-  //주어진 id값으로 포스트를 찾음
-  //파라미터로 받아 온 값은 문자열 형식이므로 숫자로 변환하거나 비교할 값을 문장려로 변경해야함.
-  const post = posts.find((p) => p.id.toString === id);
-  //포스트가 없다면 오류 반환
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
+  try {
+    //특정 id를 가진 데이터 조회 : findById()
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; //Not found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
 };
 
 /* 특정 포스트 제거
 DELETE /api/posts/:id
 */
-export const remove = (ctx) => {
-  const { id } = ctx.params;
-  //해당 id를 가진 post가 몇번째인지 확인
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  //포스트가 없으면 오류 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  //index번째 아이템을 제거
-  posts.splice(index, 1);
-  ctx.status = 204; //No Content;
-};
-
-/* 포스트 수정(교체)
-PUT /api/posts/:id
-{title, body}
-*/
-export const replace = (ctx) => {
-  //PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통쨰로 교체할 때 사용
-  const { id } = ctx.params;
-  //해당 id를 가진 post가 몇 번째인지 확인
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  //포스트가 없으면 오류 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  //전체 객체를 덮어씌움
-  //따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듦
-  posts[index] = {
-    id,
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
+export const remove = (ctx) => {};
 
 /* 포스트 수정(특정 필드 변경)
 PATCH /api/posts/:id
 {title, body}
 */
-export const update = (ctx) => {
-  //PATCH 메서드는 주어진 필드만 교체
-  const { id } = ctx.params;
-  //해당 id를 가진 post가 몇 번째인지 확인
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  //포스트가 없으면 오류 반환
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  //기존 값에 정보를 덮어 씌움
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
-};
+export const update = (ctx) => {};
