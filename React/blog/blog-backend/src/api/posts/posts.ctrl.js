a3472d08a58ebf796c9179f9a5145cd9fd96bc25
wish0ne <wish@khu.ac.kr>
2022-05-20 21:10:37 +0900

    MongoDB 연동 : 요청 검증

--- React/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ React/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,18 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+//id가 올바른 ObjectId 형식인지 검증하는 미들웨어 생성
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400; //Bad Request
+    return;
+  }
+  return next();
+};
 
 /*포스트 작성
 POST /api/posts
@@ -9,6 +23,21 @@ POST /api/posts
 }
 */
 export const write = async (ctx) => {
+  //request body 검증 추가
+  const schema = Joi.object().keys({
+    //객체가 다음 필드를 가지고 있음을 검증
+    title: Joi.string().required(), //required가 있으면 필수 항목
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(), //문자열로 이루어진 배열
+  });
+
+  //검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; //Bad Request
+    ctx.body = result.error;
+    return;
+  }
   const { title, body, tags } = ctx.request.body;
   //인스턴스 생성
   const post = new Post({
@@ -79,8 +108,25 @@ PATCH /api/posts/:id
 }
 */
 export const update = async (ctx) => {
+  //request body 검증 추가
+  const schema = Joi.object().keys({
+    //write의 schema와 유사하지만 required가 없음
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  //검증하고 나서 검증 실패인 경우 에러 처리
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400; //Bad Request
+    ctx.body = result.error;
+    return;
+  }
+
   const { id } = ctx.params;
   try {
+    //findByIdAndUpdate(id, 업데이트 내용, 업데이트 옵션)
     const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
       new: true, //이 값을 설정하면 업데이트된 데이터 반환
       //false일때는 업데이트 되기 전의 데이터 반환
